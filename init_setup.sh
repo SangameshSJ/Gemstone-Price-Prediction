f2ea082464e81f4f718be085ffab4a46cfa0a183
SangameshSJ <jssangamesh01@gmail.com>
2024-11-20 19:26:35 +0530

    latest

--- init_setup.sh
+++ init_setup.sh
@@ -1,18 +1,18 @@
+#!/bin/bash
+
 echo [$(date)]: "START"
 
+echo [$(date)]: "creating virtual environment with python 3.8 version"
 
-echo [$(date)]: "creating env with python 3.8 version" 
+python3.8 -m venv env || { echo "Python 3.8 not found. Please install it."; exit 1; }
 
+echo [$(date)]: "activating the environment"
 
-conda create --prefix ./env python=3.8 -y
+source env/bin/activate || { echo "Failed to activate the virtual environment."; exit 1; }
 
+echo [$(date)]: "installing the dev requirements"
 
-echo [$(date)]: "activating the environment" 
+pip install --upgrade pip  # Upgrade pip to the latest version
+pip install -r requirements_dev.txt || { echo "Failed to install requirements. Check the file."; exit 1; }
 
-source activate ./env
-
-echo [$(date)]: "installing the dev requirements" 
-
-pip install -r requirements_dev.txt
-
-echo [$(date)]: "END" 
+echo [$(date)]: "END"
